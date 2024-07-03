562171d9ffc96fc2c87b7dc917c3efcf534a7923
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-04 05:10:43 +0530

    added saathi logo in navigation bar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,6 +5,8 @@ import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 import { ThemeSwitcher } from './theme-switcher'
 import { Saathilogo } from './saathilogo'
+import ss from '../assets/ss.png'
+import Image from 'next/image'
 export default function Nav() {
     const path = usePathname()
 
@@ -30,7 +32,10 @@ export default function Nav() {
     return (
         <div className="fixed left-0 top-5 z-50 w-full">
             <nav className="text-text border-border dark:border-darkBorder shadow-light dark:shadow-dark mx-auto flex w-max gap-5 rounded-base border-2 bg-main p-2.5 px-5 text-sm font-base sm:text-base w450:gap-4">
-                <Saathilogo/>
+
+                <div  className="fixed left-10 top-10 z-50">
+                    <Image src={ss} width={80} height={80} alt='saathi logo'></Image>
+                </div>
                 {links.map((link) => {
                     return (
                             <Link
