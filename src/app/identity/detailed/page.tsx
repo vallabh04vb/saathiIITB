e2f7510c68539793440e6223a6d3c083708ae429
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-03 22:48:07 +0530

    added basics of gender and sexuality

--- src/app/identity/detailed/page.tsx
+++ src/app/identity/detailed/page.tsx
@@ -3,8 +3,8 @@
 import { useState } from 'react'
 
 import Tabs from '@/components/Tabs'
-
-
+import img from '@/assets/gender-bread.webp'
+import Image from 'next/image'
 
 
 export default function Detailed() {
@@ -18,79 +18,94 @@ export default function Detailed() {
     return (
 
         <>
-              <section className="bg-purple-200 w-full md:h-full snap-start snap-always shrink-0 ">
-              <div className="px-5 md:px-24 h-full py-20 2xl:p-32 ">
-          <h2 className="text-6xl md:text-7xl mb-12 tracking-tight font-bold">
-            Identities in detail
-          </h2>
-          <div className="flex flex-col md:flex-row justify-between items-center  border-12 ">
-          <div className=" rounded-base w-full">
-                <Tabs
-                    activeTab={activeTab}
-                    setActiveTab={setActiveTab}
-                    tabsArray={tabsArray}
-                />
-                <div className="max-w-full rounded-b-base border-2 border-border dark:border-darkBorder bg-white dark:bg-darkBg p-5 font-base">
-                    {activeTab === 'Gay/Lesbian' && (
-                        <div>
-                            <h3>Tab 1</h3>
+            <section className="bg-purple-200 w-full md:h-full snap-start snap-always shrink-0 ">
+                <div className="px-5 md:px-24 h-full py-20 2xl:p-32 ">
+                    <h2 className="text-6xl md:text-7xl mb-12 tracking-tight font-bold">
+                        Identities in detail
+                    </h2>
+                    <div className="flex flex-col md:flex-row justify-between items-center  border-12 ">
+                        <div className=" rounded-base w-full">
+                            <Tabs
+                                activeTab={activeTab}
+                                setActiveTab={setActiveTab}
+                                tabsArray={tabsArray}
+                            />
+                            <div className="max-w-full rounded-b-base border-2 border-border dark:border-darkBorder bg-white dark:bg-darkBg p-5 font-base">
+                                {activeTab === 'Gay/Lesbian' && (
+                                    <div>
+                                        <h3>Tab 1</h3>
 
-                            <p className="mt-2">Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    )}
-                    {activeTab === 'Bi/Pan' && (
-                        <div>
-                            <h3>Tab 2</h3>
+                                        <p className="mt-2">Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                )}
+                                {activeTab === 'Bi/Pan' && (
+                                    <div>
+                                        <h3>Tab 2</h3>s
 
-                            <p className="mt-2">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    )}
-                    {activeTab === 'Trans' && (
-                        <div>
-                            <h3>Tab 3</h3>
+                                        <p className="mt-2">
+                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                )}
+                                {activeTab === 'Trans' && (
+                                    <div>
+                                        <h3>Tab 3</h3>
 
-                            <p className="mt-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-                                necessitatibus!
-                            </p>
-                        </div>
-                    )}
-                    {activeTab === 'Ace/Aro' && (
-                        <div>
-                            <h3>Tab 1</h3>
+                                        <p className="mt-2">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+                                            necessitatibus!
+                                        </p>
+                                    </div>
+                                )}
+                                {activeTab === 'Ace/Aro' && (
+                                    <div>
+                                        <h3>Tab 1</h3>
 
-                            <p className="mt-2">Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    )}
-                    {activeTab === 'Non Binary' && (
-                        <div>
-                            <h3>Tab 2</h3>
+                                        <p className="mt-2">Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                )}
+                                {activeTab === 'Non Binary' && (
+                                    <div>
+                                        <h3>Tab 2</h3>
 
-                            <p className="mt-2">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    )}
-                    {activeTab === 'Intersex' && (
-                        <div>
-                            <h3>Tab 3</h3>
+                                        <p className="mt-2">
+                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                        </p>
+                                    </div>
+                                )}
+                                {activeTab === 'Intersex' && (
+                                    <div>
+                                        <h3>Tab 3</h3>
 
-                            <p className="mt-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-                                necessitatibus!
-                            </p>
+                                        <p className="mt-2">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+                                            necessitatibus!
+                                        </p>
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                    )}
+                    </div>
                 </div>
-            </div>
-            </div>
-            </div>
+            </section>
 
-
-
-              </section>
+            <section className="bg-purple-200 w-full md:h-full snap-start snap-always shrink-0 ">
+                <div className="px-5 md:px-24 h-full py-20 2xl:p-32 space-y-9">
+                    <h2 className="text-6xl md:text-7xl mb-12 tracking-tight font-bold">
+                        Basics Of Gender And Sexuality
+                    </h2>
+                    <div className=" justify-between items-center  border-12 margin">
+                        <p>The following are a starting point to understand Gender and Sexuality. It might be more complicated as you study more about it but the basics are simple enough.</p>
+                        <p><b className='md:text-2xl'>Sex:</b> The classification of a person as male or female. At birth, infants are assigned a sex, usually based on the appearance of their external anatomy. (This is what is written on the birth certificate.) A person's sex, however, is actually a combination of bodily characteristics including: chromosomes, hormones, internal and external reproductive organs, and secondary sex characteristics.</p>
+                        <p><b className='md:text-2xl'>Gender Identity:</b> A person's internal, deeply held sense of their gender. For transgender people, their own internal gender identity does not match the sex they were assigned at birth. Most people have a gender identity of man or woman (or boy or girl). For some people, their gender identity does not fit neatly into one of those two choices (see non-binary and/or genderqueer below.) Unlike gender expression (see below) gender identity is not visible to others.</p>
+                        <p><b className='md:text-2xl'>Gender Expression:</b> External manifestations of gender, expressed through a person's name, pronouns, clothing, haircut, behavior, voice, and/or body characteristics. Society identifies these cues as masculine and feminine, although what is considered masculine or feminine changes over time and varies by culture. Typically, transgender people seek to align their gender expression with their gender identity, rather than the sex they were assigned at birth.</p>
+                        <p><b className='md:text-2xl'>Sexual Orientation:</b> The scientifically accurate term for an individual's enduring physical, romantic and/or emotional attraction to members of the same and/or opposite sex, including lesbian, gay, bisexual and heterosexual (straight) orientations.</p>
+                    </div>
+                    <div className="flex  justify-center items-center mt-0.5">
+                        <Image src={img} width={800} height={1000} alt="fsdf" />
+                    </div>
+                </div>
+            </section>
 
         </>
 
