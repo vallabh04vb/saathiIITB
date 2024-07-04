9e520116c02be3946978c64d9b146b6e1acf476c
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-04 08:42:44 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const dmSans = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Saathi IITB',
+  description: 'Offcial Website of Saathi IIT Bombay',
+  icons:{
+    icon:['/fav/favicon.ico'],
+    apple:['/fav/apple-touch-icon.png'],
+    shortcut:['/fav/apple-touch-icon.png']
+  }
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Features from '@/sections/features'
 import Header from '@/sections/header'
 import Faq from '@/sections/faq'
 import Footer from '@/components/footer'
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-    
+    <Head>
+      <link rel="icon" href="/favicon.ico" sizes='any'> </link>
+    </Head>
       <Header />
       <Features />
       <Community />
