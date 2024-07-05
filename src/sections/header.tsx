472e26d39855024f158d098c99e9d42c6068ee1f
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-05 21:00:01 +0530

    added link to more about us button to route to /about page

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import Saathilogo  from '@/assets/saathi.png'
+import Saathilogo from '@/assets/saathi.png'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className="dark:bg-darkBg inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
-        
+
         <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl">
           <Image src={Saathilogo} width={5000} height={5000} alt='sfcfsdf'></Image>
         </h1>
@@ -14,12 +15,14 @@ export default function Header() {
 
           Saathi is IIT Bombay&349;s LGBTQ+ resource group. It was founded in 2011 to raise awareness on issues relating to the LGBTQ+ community and advocate for an inclusive campus experience for all. We are the oldest queer body in the IITs and one of the oldest across India :)
         </p>
-        <Button
-          size="lg"
-          className="h-12 text-base font-heading md:text-lg lg:h-14 lg:text-xl"
-        >
-          More About Us
-        </Button>
+        <Link href={'/about'}>
+          <Button
+            size="lg"
+            className="h-12 text-base font-heading md:text-lg lg:h-14 lg:text-xl"
+          >
+            More About Us
+          </Button></Link>
+
       </div>
     </header>
   )
